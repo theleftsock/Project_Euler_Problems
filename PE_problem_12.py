17d082406b6d74e359cd8ea728aa1b95aa9ecfc1
theleftsock <themutesock@gmail.com>
2014-10-14 06:41:54 -0400

    test commit changes to problem 12 file

--- PE_problem_12.py
+++ PE_problem_12.py
@@ -15,6 +15,7 @@ Let us list the factors of the first seven triangle numbers:
 We can see that 28 is the first triangle number to have over five divisors.
 What is the value of the first triangle number to have over five hundred divisors?
 """
+# this is a test comment for pushing to the repo
 
 from project_euler_libs import *
 
